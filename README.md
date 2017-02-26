96877301656dd15b37c3b6e784bb0c802ce9e0ff
rcasaroto <rodrigocasaroto@gmail.com>
2017-02-26 02:22:14 -0300

    Inclusão do projeto web no qual rodará a camada SPA

--- README.md
+++ README.md
@@ -11,10 +11,18 @@
 - Testes unitários utilizando PowerMockito
 
 ## Proposta de criação inicial do projeto
-- Criação interativa de uma prova de conceito com interface web simples acessando uma API REST inicialmente mockada no
+- Criação interativa de uma prova de conceito com interface web simples acessando uma API REST inicialmente mockada 
 - Utilizar TDD e criar testes unitários conforme elabora a API
 - Modelar as demais camadas do Backend conforme melhor elucidação dos requisitos
 - Em uma etapa posterior onde os requisitos/proposta de projeto estiver mais madura criar e integrar com o banco de dados
+
+## Arquitetura de módulos
+- Projeto SPA deployado em war JEE rodando em servidor JBOSS
+- Projeto Backend deployado em war JEE rodando em servidor JBOSS
+
+## Ambiente de desenvolvimento
+- Servidor: JBOSS EAP 6.1
+- IDE Eclipse
 
 ## Ambiente cloud escolhido para live preview do projeto
 - AWS
@@ -22,7 +30,7 @@
 ## Github
 https://github.com/casaroto/bluebank
 
-## Instruções para deploy e execução.
+## Instruções para deploy e execução
 - [TODO]
 
 
--- javaee/Bluebankend/src/br/com/bluebank/controller/BoasVindasController.java
+++ javaee/Bluebankend/src/br/com/bluebank/controller/BoasVindasController.java
@@ -1,21 +1,63 @@
 package br.com.bluebank.controller;
-
+/*
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;*/
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
  
-@Controller
+@RestController
+@RequestMapping("/service/")
 public class BoasVindasController {
-
-
-	@RequestMapping(value = "/boasvindas", method = RequestMethod.GET)
-	public @ResponseBody String ola() {
- 
-		String message = "ola";
 	
+	@RequestMapping(value = "/boasvindas", method = RequestMethod.GET,headers="Accept=application/json")
+	public Mensagem getUser() {
+		//User user=userService.getUserById(id);
+		
+		return new Mensagem();
+	}
+	
+	//@RequestMapping("/boasvindas", produces="application/json")
+	//@RequestMapping("/boasvindas")
+	//public @ResponseBody Mensagem ola() {
+	//@RequestMapping(value = "/boasvindas", method = RequestMethod.GET, produces = "application/json")
+	/*@RequestMapping(value = "/boasvindas", method = RequestMethod.GET)
+	public String ola() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>OLá</h3>**********</div><br><br>";
+		//return new ModelAndView("boasvindas", "message", message);
 		
 		return message;
+	}*/
+
+	//@RequestMapping(value = "/boasvindas", method = RequestMethod.GET, produces = "application/json")
+	/*@RequestMapping(value="/rest", produces = "application/json")
+	public String ola() {
+ 
+		
+		//String message = "ola";
+		System.out.print("testeer $$$$$$$$$$$$$");
+		
+		//return new Mensagem();
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>OLá</h3>**********</div><br><br>";
+		return "{\"teste\";10}";
 	}
+	
+	
+	@RequestMapping("/bova")
+	public ModelAndView ola2() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>OLá</h3>**********</div><br><br>";
+		return new ModelAndView("boasvindas", "message", message);
+	}*/
 }
