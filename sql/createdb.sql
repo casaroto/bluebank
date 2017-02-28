d0308a4bc67edffd0dedfef05a07c90c588c7d28
casaroto <rodrigocasaroot@gmail.com>
2017-02-28 13:06:18 -0300

    Incremento da modelagem das tabelas do sistema

--- sql/createdb.sql
+++ sql/createdb.sql
@@ -1,4 +1,10 @@
 
+USE bluebank;
+
+DROP TABLE correntista;
+DROP TABLE conta;
+DROP TABLE saldo;
+DROP TABLE transferencia;
 
 CREATE TABLE `correntista` (
   `idcorrentista` int(11) NOT NULL,
@@ -6,7 +12,6 @@ CREATE TABLE `correntista` (
   `cpfCnpj` varchar(14) DEFAULT NULL,
   PRIMARY KEY (`idcorrentista`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
 
 
 CREATE TABLE `conta` (
@@ -18,6 +23,31 @@ CREATE TABLE `conta` (
   `idcorrentista` int(11) DEFAULT NULL,
   PRIMARY KEY (`idconta`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `saldo` (
+  `idsaldo` int(11) NOT NULL,
+  `idcorrentista` int(11) DEFAULT NULL,
+  `valor` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idsaldo`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `transferencia` (
+  `idtransferencia` int(11) NOT NULL,
+  `idcorrentista_origem` int(11) DEFAULT NULL,
+  `idcorrentista_destino` int(11) DEFAULT NULL,
+  `data` datetime DEFAULT NULL,
+  `efetuada` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idtransferencia`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+insert into correntista values (1, 'Ana Maria','58424255135'); 
+insert into correntista values (2, 'Maria Luiza','38521876300'); 
+insert into correntista values (3, 'Osvaldo Silva','33165275670'); 
+
+
 
 
 
